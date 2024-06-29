1b4b6b0f32238c3e351a3b8461f804adb1aac3e5
LahavHarary <lahavitsme0@gmail.com>
2024-06-29 17:11:45 +0300

    added a new anime genre

---
+++ src/database/scripts/clean-collection.ts
@@ -0,0 +1,16 @@
+import { connect } from '../db';
+
+const collectionName = "movie_genres";
+
+async function cleanCollection() {
+    const db = await connect();
+    const collection = db.collection(collectionName);
+
+    const result = await collection.deleteMany({});
+    console.log(`Deleted ${result.deletedCount} documents from ${collectionName}`);
+
+    console.log(`Collection '${collectionName}' cleaned successfully`);
+    //await client.close();
+}
+
+cleanCollection().catch(console.error);
