adef60df87d9e7cf2983202ad40ac352cdfd5ff5
LahavHarary <lahavitsme0@gmail.com>
2024-07-07 19:18:49 +0300

    Added movies controller, route and a script to add movies to the mongodb. also implemented a disconned logic for the db class

---
+++ private-scripts/change-git-to-private.sh
@@ -0,0 +1,4 @@
+git config --global user.name "LahavHarary"
+git config --global user.email "lahavitsme0@gmail.com"
+git config user.name "LahavHarary"
+git config user.email "lahavitsme0@gmail.com"
