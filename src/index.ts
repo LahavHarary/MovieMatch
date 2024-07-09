c770f28ac78b11beb9a3ee452dbc7b0eca7db4e5
Lahav Fibonatix <Lahav.Harary@fibonatix.com>
2024-07-09 20:53:36 +0300

    added an admin controller to handle adding movies to database, first version

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import movieGenresRoutes from './routes/movie-genres-routes';
 import moviesRoutes from './routes/movies-routes';
+import adminRoutes from './routes/admin-routes';
 
 const app = express();
 const port = 3000;
@@ -9,6 +10,7 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use('/api', moviesRoutes);
 app.use('/api', movieGenresRoutes);
+app.use('/admin', adminRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
