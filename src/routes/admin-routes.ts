408cce9a0a73870e298000bb4f58af5a591c0bf3
Lahav Harary <70967259+LahavHarary@users.noreply.github.com>
2024-07-10 20:46:24 +0300

    Revert "(main) task/7-create-an-admin-controller - added an admin controller to handle adding movies to database, first version"

--- src/routes/admin-routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { AddMovie } from '../controllers/admin-controller';
-
-const router = Router();
-
-router.post('/add-movie', AddMovie);
-
-export default router;
