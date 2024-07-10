df2653fd8d7aa5d546e51723b11410901da25ff1
Lahav Harary <70967259+LahavHarary@users.noreply.github.com>
2024-07-10 20:46:24 +0300

    Revert "(main) task/7-create-an-admin-controller - added an admin controller to handle adding movies to database, first version"

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import movieGenresRoutes from './routes/movie-genres-routes';
 import moviesRoutes from './routes/movies-routes';
-import adminRoutes from './routes/admin-routes';
 
 const app = express();
 const port = 3000;
@@ -10,7 +9,6 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use('/api', moviesRoutes);
 app.use('/api', movieGenresRoutes);
-app.use('/admin', adminRoutes);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
